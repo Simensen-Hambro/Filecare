4bbcd916cbe75165e0baf0ec4523aad401178dc8
Carl Johan Hambro <carljohan.hambro@gmail.com>
2017-06-06 22:06:12 +0200

    Replace string operations on paths with built in functions

--- portal/models.py
+++ portal/models.py
@@ -1,4 +1,5 @@
 import os
+from os.path import join
 from uuid import uuid4
 
 from django.conf import settings
@@ -25,27 +26,27 @@ class SharedNode(models.Model):
     objects = SharedNodeManager()
 
     def __str__(self):
-        return f'{self.token.hex}'
+        return self.token.hex
 
     def delete(self, *args, **kwargs):
-        os.unlink(f'{settings.ROOT_SHARE_PATH}/{self}/{os.path.basename(self.node.absolute_path)}')
-        os.rmdir(f'{settings.ROOT_SHARE_PATH}/{self}')
+        os.unlink(join(settings.ROOT_SHARE_PATH, str(self), os.path.basename(self.node.absolute_path)))
+        os.rmdir(join(settings.ROOT_SHARE_PATH, str(self)))
         super(SharedNode, self).delete(*args, **kwargs)
 
     def save(self, *args, **kwargs):
         if not self.pk:
             # Instance was newly created
-            path = f'{settings.ROOT_SHARE_PATH}/{self}'
+            path = join(settings.ROOT_SHARE_PATH, str(self))
 
             if not os.path.isdir(path):
                 os.makedirs(path)
 
-            os.symlink(f'{self.node.absolute_path}',
-                       f'{settings.ROOT_SHARE_PATH}/{self}/{os.path.basename(self.node.absolute_path)}')
+            os.symlink(self.node.absolute_path, join(settings.ROOT_SHARE_PATH, str(self),
+                                                     os.path.basename(self.node.absolute_path)))
         super(SharedNode, self).save(*args, **kwargs)
 
     def get_child_url(self, node):
         parent_abs_path = self.node.absolute_path
         child_abs_path = node.absolute_path
-        relative_path = os.path.realpath(parent_abs_path, child_abs_path)
-        return f'{os.path.join(settings.ROOT_SHARE_PATH, str(self), relative_path)}'
+        relative_path = os.path.relpath(join(parent_abs_path, str(self)), child_abs_path)
+        return os.path.join(settings.ROOT_SHARE_PATH, relative_path)
