f21515b634684f44b0ee65d91383885d9621dda3
Carl Johan Hambro <carljohan.hambro@gmail.com>
2017-06-15 13:00:02 +0200

    Improve serializers for Node and SharedNode

--- portal/serializers.py
+++ portal/serializers.py
@@ -3,18 +3,21 @@ from rest_framework import serializers
 from portal.models import SharedNode, Node
 
 
+class NodeSerializer(serializers.ModelSerializer):
+    children = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = Node
+        fields = ('size', 'id', 'children')
+
+
 class ShareSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(
         read_only=True,
         default=serializers.CurrentUserDefault()
     )
+    node = NodeSerializer(many=False)
 
     class Meta:
         model = SharedNode
         fields = ('token', 'expiration', 'node', 'user')
-
-
-class NodeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Node
-        fields = ('size', 'uuid')
